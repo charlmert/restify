0ebc17d7a1e6373757b343cea53619e9174319a7
Charl Joseph Mert <charl.mert@gmail.com>
2013-01-29 13:48:50 +0200

    changed imports to local

--- restify/restify.py
+++ restify/restify.py
@@ -1,10 +1,8 @@
-import restify.introspection as introspection
-from restify.services import RestInterface
+import introspection
+from services import RestInterface
 # not all objects have a name but those that do store it in the __name__ attribute.
 def main():
     introspection.get_callables('RestInterface','^_.*')
 
 if __name__ == '__main__':
     main()
-
-
